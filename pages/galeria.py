c25800c68ba9fc50040d52742327c1bcd4128b9f
Nhadorgue <gabrielmoura.angelo@gmail.com>
2026-02-01 13:58:33 -0300

    criacao ao inicio a pagina de presentes

--- pages/galeria.py
+++ pages/galeria.py
@@ -2,8 +2,6 @@ import streamlit as st
 from utils.background import apply_virgem_maria_background
 from utils.gallery import get_gallery_images
 
-COLS = 4  # 4 colunas x 3 linhas = 12 fotos
-
 def render():
     st.markdown(apply_virgem_maria_background(), unsafe_allow_html=True)
 
@@ -13,44 +11,20 @@ def render():
     # ---------- IGREJA ----------
     st.subheader("📸 O lugar onde tudo acontecerá")
 
-    # col1, col2, col3 = st.columns(3)
+    col1, col2, col3 = st.columns([0.5, 4, 0.5])
 
-    # with col1:
-    #     st.image("assets/images/externa.jpg", caption="Paróquia - Visão externa")
-    # with col2:
-    #     st.image("assets/images/interior.jpg", caption="Interior da igreja")
-    # with col3:
-    #     st.image("assets/images/altar.jpg", caption="Altar")
 
-    # col1 = st.columns([1])
-
-    # with col1:
-    st.image(
-        "assets/images/igreja_vertical.jpg",
-        width='stretch'
-    )  
+    with col2:
+        st.image(
+            "assets/images/igreja_vertical.jpg",
+            width='stretch'
+        )  
 
     st.divider()
 
     # ---------- GALERIA DO CASAL ----------
 
     st.subheader("👀 Algumas fotinhas nossas e participações especiais...🤍")
-
-    # images = get_gallery_images()
-
-    # if not images:
-    #     st.info("📷 Em breve novas fotos...")
-    #     return
-
-    # # 🔒 Limite fixo: no máximo 12 imagens
-    # gallery_images = images[:12]
-
-    # # ---------- GRID FIXO 4x3 ----------
-    # for row in range(0, len(gallery_images), COLS):
-    #     cols = st.columns(COLS)
-    #     for col, img_path in zip(cols, gallery_images[row:row + COLS]):
-    #         with col:
-    #             st.image(img_path)
 
     col1, col2, col3 = st.columns([1, 4, 1])
 
--- pages/presentes.py
+++ pages/presentes.py
@@ -4,5 +4,12 @@ from utils.background import apply_virgem_maria_background
 def render():
     st.markdown(apply_virgem_maria_background(), unsafe_allow_html=True)
     
-    st.title("🎁 Presentes")
-    st.write("Em breve todas as informações sobre o grande dia 🤍")
+    st.title("🎁 Lista de Presentes")
+    st.markdown("""
+    Com muito carinho, preparamos esta lista para facilitar a vida de quem deseja nos presentear 🤍  
+
+    Caso surja qualquer dúvida — sobre os presentes, valores ou formas de contribuição — fiquem totalmente à vontade para entrar em contato diretamente conosco.  
+    Que Deus abençoe cada gesto de carinho 🙏
+    """)
+
+    st.divider()
